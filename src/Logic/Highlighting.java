665a136904405c2afd942407315cac0805edc708
Max Goodman <maximgoodman@gmail.com>
2019-12-10 07:22:55 -0600

    implemented rook movement rules

--- src/Logic/Highlighting.java
+++ src/Logic/Highlighting.java
@@ -59,7 +59,7 @@ public class Highlighting {
     }
     public Tile[][] rookRules(Tile[][] board, Piece piece){
 
-        return straightColsContact(board, piece);
+        return sideColsContact(straightColsContact(board, piece),piece);
     }
 
 
@@ -67,7 +67,7 @@ public class Highlighting {
         int col = piece.getCol();
         int row = piece.getRow();
 
-        for(int i =col; i<board[row].length; i++){
+        for(int i =row+1; i<board.length; i++){
             if(board[i][col].getPiece()!=null&&board[i][col].getPiece().getSide()!=piece.getSide()){
                 board[i][col].setBackground(Color.RED);
                 board[i][col].setSelected(true, piece);
@@ -81,7 +81,7 @@ public class Highlighting {
                 break;
             }
         }
-        for(int i =col; i>=0; i--){
+        for(int i =row-1; i>=0; i--){
             if(board[i][col].getPiece()!=null&&board[i][col].getPiece().getSide()!=piece.getSide()){
                 board[i][col].setBackground(Color.RED);
                 board[i][col].setSelected(true, piece);
@@ -101,10 +101,10 @@ public class Highlighting {
     }
 
     private Tile[][] sideColsContact(Tile[][] board, Piece piece){
-        //int col = piece.getCol();
+        int col = piece.getCol();
         int row = piece.getRow();
 
-        for(int i = row; i<board.length; i++){
+        for(int i = col+1; i<board.length; i++){
             if(board[row][i].getPiece()!=null&&board[row][i].getPiece().getSide()!=piece.getSide()){
                 board[row][i].setBackground(Color.RED);
                 board[row][i].setSelected(true, piece);
@@ -118,7 +118,7 @@ public class Highlighting {
                 break;
             }
         }
-        for(int i =row; i>=0; i--){
+        for(int i =col-1; i>=0; i--){
             if(board[row][i].getPiece()!=null&&board[row][i].getPiece().getSide()!=piece.getSide()){
                 board[row][i].setBackground(Color.RED);
                 board[row][i].setSelected(true, piece);
